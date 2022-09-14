dc9ad7968a9a06d741d80af098bcafcd60c79006
Thijmen Makkinga <s1172262@student.windesheim.nl>
2022-09-14 22:18:08 +0200

    initial commit

---
+++ week 1/week 1 opdracht 2.19/Source.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	printf("Enter three different integers: ");
+	scanf_s("%d%d%d", &a, &b, &c);
+
+	int sum;
+		sum=a+b+c;
+	printf("Sum is %d\n", sum);
+	
+	int avg;
+	avg = (a + b + c) / 3;
+	printf("Average is %d\n", avg);
+
+	int product;
+	product = a * b * c;
+	printf("Product is %d\n", product);
+
+	int smallest;
+	smallest = a;
+	if (b < smallest){
+		b = smallest;
+	}
+	if (c < smallest){
+		c = smallest; 
+	}
+			printf("Smallest is %d\n", smallest);
+		
+			int largest;
+			largest = a;
+			if (b > largest){
+				largest = b;
+			}
+				if (c > largest){
+					largest = c;
+				}
+				printf("Largest is %d", largest);
+			
+	}
+
+	
